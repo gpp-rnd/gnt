6d746ffe51e372cb0a3fdb9587a578f75a8fe402
PeterDeWeirdt <petedeweirdt@gmail.com>
2020-08-19 09:28:28 -0400

    Bump version: 0.3.1 → 0.3.2

--- gnt/__init__.py
+++ gnt/__init__.py
@@ -3,4 +3,4 @@ from .score import get_guide_residuals, get_gene_residuals, get_guide_dlfcs, get
 
 __author__ = """Peter C. DeWeirdt"""
 __email__ = 'petedeweirdt@gmail.com'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
